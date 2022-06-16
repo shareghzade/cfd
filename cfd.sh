3e1e57c9134b2351680d2f228bc97cadded0c3cb
shareghzade <shareghzade@gmail.com>
2022-06-16 16:01:18 +0430

    input loop added

--- cfd.sh
+++ cfd.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-# this program will create file for you
-# and if file does exists its not gonna create it
-# you can create only files not directories
-
+# with this program you can create file and directory
+# you can just run the programm and it will prompt you
+# or you can use the arguments
+# ./cfd.sh file <file_name>
+# ./cfd.sh dir <dir_name>
 
 
 # with this if statement user can't run the program as root
@@ -26,36 +27,38 @@ elif [[ $1 == "dir" ]] && [[ -n $2 ]]; then
 	echo -e "\n\e[32mDirectory ${2} Created\n"
 else
 	read -p "Do you want file or directory? file/dir: " cfd
-
 	case ${cfd} in
 		file | File | FILE | FiLe | fIlE | f | F | fil | Fil)
 
 			read -p "Please Enter File Name: " filename
-
-			if [[ -e ${filename} ]]; then # check if file already exists
-				echo -e "\n\e[31m'${filename}' File Exists\n"
-			elif [[ -z ${filename} ]]; then # check if user sending empty input
-				echo -e "\n\e[31mPlease Enter File Name\n"
-				exit 1
-			else # if everything was ok it will create the file
-				touch ${filename}
-				echo -e "\n\e[32mFile '${filename}' Created\n"
-			fi
+			while [[ -e ${filename} ]]
+			do
+				echo -e "\n\e[31m'${filename}' File Exists\n\e[0m"
+				read -p "Please Enter File Name: " filename
+			done
+			while [[ -z ${filename} ]]
+			do
+				echo -e "\n\e[31mYou Have to Enter the Name\n\e[0m"
+				read -p "Please Enter File Name: " filename
+			done
+			touch ${filename}
+			echo -e "\n\e[32mFile '${filename}' Created\n"
 			;;
-
 		dir | Dir | DIR | directory | Directory | DIRECTORY | d | D)
 
 			read -p "Please Enter Directory Name: " dirname
-
-			if [[ -d ${dirname} ]]; then # check if file exists and its a directory
-				echo -e "\n\e[31m'${dirname}' Directory Exists\n"
-			elif [[ -z ${dirname} ]]; then # check if user sending empty input
-				echo -e "\n\e[31mPlease Enter Directory Name\n"
-				exit 1
-			else # if everything was ok it will create the file
+			while [[ -d ${dirname} ]]
+			do
+				echo -e "\n\e[31m'${dirname}' Directory Exists\n\e[0m"
+				read -p "Please Enter Directory Name: " dirname
+			done
+			while [[ -z ${dirname} ]]
+			do
+				echo -e "\n\e[31mPlease Enter Directory Name\n\e[0m"
+				read -p "Please Enter Directory Name: " dirname
+			done
 				mkdir -p ${dirname} # -p let you to create multiple directory in a row. example: /test1/test2/test3
-				echo -e "\n\e[32mDirectory '${dirname}' Created\n"
-			fi
+				echo -e "\n\e[32mDirectory '${dirname}' Created\n\e[0m"
 			;;
 
 		*)
@@ -63,5 +66,4 @@ else
 			echo -e "\n\e[32mUsage:\n\tbash $0 file <file_name>\n\tbash $0 dir <dir_name>\n\n\tor\n\n\t${0} file <file_name>\n\t${0} dir <dir_name>"
 
 	esac
-
 fi
